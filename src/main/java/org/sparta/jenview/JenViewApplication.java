2cfdd6dac5cda44ef008de2399c5d4095d534e63
jenny <wodms1474@gmail.com>
2024-06-24 18:07:22 +0900

    레디스 콘픽 플레이스 홀더 수정

--- src/main/java/org/sparta/jenview/JenViewApplication.java
+++ src/main/java/org/sparta/jenview/JenViewApplication.java
@@ -2,8 +2,10 @@ package org.sparta.jenview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan
 public class JenViewApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/sparta/jenview/dto/JenViewResponseDto.java
+++
@@ -1,4 +0,0 @@
-package org.sparta.jenview.dto;
-
-public class JenViewResponseDto {
-}
